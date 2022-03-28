6e49d616da0ae038fe6a9a8dd17d3b9122dcbe41
Rebekah Zhou <rebekahzhou@gmail.com>
2022-03-28 14:15:35 -0700

    add login/logout backend, connect with frontend

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -3,7 +3,37 @@ import { useState } from 'react'
 import logo from '../logo.svg'
 
 function Login() {
-  const [showLogin, setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(true)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+  function handleLoginSubmit(e) {
+    e.preventDefault() 
+    fetch('/login', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((r) => r.json())
+      .then((user) => console.log(`${user.name} is logged in!`)) //change console.log here to do something upon logged on
+  }
+
+  function handleSignupSubmit(e) {
+    e.preventDefault() 
+    fetch('/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password, password_confirmation: passwordConfirmation }),
+    })
+      .then((r) => r.json())
+      .then((newUser) => console.log(newUser)) //change console.log here to do something upon logged on
+  }
 
   return (
     <>
@@ -12,27 +42,59 @@ function Login() {
     <div class="forms">
     {showLogin ?
     <form id="login">
-          <h1>Login</h1>
+          <h1>Sign in to Twiddle Wakka</h1>
           <div class="input-field">
             <label for="email">Email</label>
-            <input type="email" name="email"  />
+            <input 
+              type="email" 
+              name="email"  
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
             <label for="password">Password</label> 
-            <input type="password" name="password" />
-            <input type="submit" value="Login" class="button"/>
-            <button onClick={() => setShowLogin(!showLogin)}>Sign up!</button>
+            <input 
+              type="password" 
+              name="password" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <input onClick={handleLoginSubmit} type="submit" value="Login" class="button"/>
+            <p>Don't have an account? <button onClick={() => setShowLogin(!showLogin)}>Sign up</button></p>
           </div>
       </form> :
       <form id="signup">
-          <h1>Sign Up</h1>
+          <h1>Join Twiddle Wakka today</h1>
           <div class="input-field">
+          <label for="name">Name</label> 
+            <input 
+              type="text" 
+              name="name"  
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
             <label for="email">Email</label> 
-            <input type="email" name="email" />
+            <input 
+              type="text" 
+              name="email"  
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
             <label for="password">Password</label> 
-            <input type="password" name="password" />
-            <label for="password">Confirm Password</label> 
-            <input type="password" name="password" />
-            <input type="submit" value="Sign up" class="button" />
-            <p>Already have an account? <button onClick={() => setShowLogin(!showLogin)}>Sign in here</button></p>
+            <input 
+              type="password" 
+              name="password" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <label for="password_confirmation">Confirm Password</label> 
+            <input 
+              type="password" 
+              name="password_confirmation" 
+              value={passwordConfirmation}
+              onChange={e => setPasswordConfirmation(e.target.value)}
+            />
+            <input onClick={handleSignupSubmit} type="submit" value="Sign up" class="button" />
+            <p>Already have an account? <button onClick={() => setShowLogin(!showLogin)}>Log in </button></p>
           </div>
       </form>
     }

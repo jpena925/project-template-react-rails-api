e8331a2244305d410fe410450cd42135e5b70d5a
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-03-30 12:50:38 -0400

    render discover page content

--- client/src/Components/Homepage/FeedItem.js
+++ client/src/Components/Homepage/FeedItem.js
@@ -6,12 +6,16 @@ import ProjectCard from './ProjectCard';
 function FeedItem({ props }) {
   const [showComments, setShowComments] = useState(false)
 
-  const commentsMap = props.comments.map(comment => <Comment 
-                            className='comment-box'
-                            key={comment.id} 
-                            text={comment.text} 
-                            user_id={comment.user_id} 
-                            name={comment.name} />)
+  const commentsMap = props.comments.map(comment => (
+      <Comment 
+        className='comment-box'
+        key={comment.id} 
+        text={comment.text} 
+        user_id={comment.user_id} 
+        name={comment.name}
+      />
+    )
+  )
 
                   
   return (
--- client/src/Components/Homepage/HomePage.js
+++ client/src/Components/Homepage/HomePage.js
@@ -1,11 +1,13 @@
 
-import React, {useContext} from 'react'
+import React, { useContext, useState } from 'react'
 import Feed from './Feed'
+import Discover from './Discover'
 import PostForm from '../PostForm.js'
 import { UserContext } from '../../App'
 import prof from '../../download (1).png'
 
 function HomePage({ profPic, setProfPic }) {
+  const [feedOrDiscover, setFeedOrDiscover] = useState("feed")
   const user = useContext(UserContext)
 
 
@@ -13,9 +15,9 @@ function HomePage({ profPic, setProfPic }) {
     <>
     <div>Welcome USERNAME!</div>
     <PostForm profPic={profPic} />
-    <button type="button" className="feed-btns">Following</button>
-    <button type="button" className="feed-btns">Discover</button>
-    <Feed />
+    <button type="button" className="feed-btns" onClick={() => setFeedOrDiscover("feed")}>Feed</button>
+    <button type="button" className="feed-btns" onClick={() => setFeedOrDiscover("discover")}>Discover</button>
+    {feedOrDiscover == "feed" ? <Feed /> : <Discover />}
     </>
   )
 }

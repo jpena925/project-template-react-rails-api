6bb191b79a9db197c503a6720a3dd806c1ecac7b
David <Davidmaxsands@gmail.com>
2022-03-31 15:40:12 -0600

    some stuff

--- client/src/Components/Profile/VisitingPage.js
+++ client/src/Components/Profile/VisitingPage.js
@@ -4,6 +4,9 @@ import prof from '../../download (1).png'
 import VisitingFeed from './VisitingFeed'
 import ProfileFeed from './ProfileFeed'
 import { UserContext } from '../../App'
+import { AiFillLinkedin } from 'react-icons/ai'
+import { BsGithub } from 'react-icons/bs'
+import { SiMedium } from 'react-icons/si'
 
 function VisitingPage() {
     const params = useParams().id
@@ -79,6 +82,8 @@ function VisitingPage() {
       setIsFollow(false)
     }
 
+    console.log(visitedUser?.blog)
+
     return (
     <div className='split'>
     <VisitingFeed profPic={profPic} />
@@ -99,10 +104,10 @@ function VisitingPage() {
             <p id='bio-text'>{visitedUser?.bio}</p>
         </div>
         <div id='links'>
-          <h2>My links:</h2>
-          <p><a href={visitedUser?.linkedin}>Linkedin</a></p>
-          <p><a href={visitedUser?.github}>Github</a></p>
-          <p><a href={visitedUser?.blog}>Blog</a></p>
+          <h2>Links:</h2>
+          {visitedUser?.linkedin === '' ? null : <p><i><AiFillLinkedin className='link-icon'/> </i><a href={visitedUser?.linkedin}>Linkedin</a></p>}
+          {visitedUser?.github === '' ? null : <p><i><BsGithub className='link-icon' /></i> <a href={visitedUser?.github}>Github</a></p>}
+          {visitedUser?.blog === '' ? null : <p><i><SiMedium className='link-icon' /></i> <a href={visitedUser?.blog}>Blog</a></p>}
         </div>
       </div>
     </div>
